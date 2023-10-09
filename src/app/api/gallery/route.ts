11312ea91802460389a478b59d6285628e686dad
Deniscley Marfran <deniscleymaf@gmail.com>
2023-10-09 10:31:01 -0300

    feat: Creating code for routes

---
+++ src/app/api/gallery/route.ts
@@ -0,0 +1,21 @@
+import { NextResponse } from "next/server";
+
+export async function GET() {
+  const res = await fetch("https://localhost:44364/api/galeria/obter-todos", {
+    mode: "no-cors",
+  });
+
+  // const res = await fetch("https://localhost:44364/api/client/obter-todos", {
+  //   mode: "no-cors",
+  // });
+
+  console.log("Resposta da requisição: ", res);
+
+  if (!res.ok) {
+    // This will activate the closest `error.js` Error Boundary
+    throw new Error("Falha ao buscar dados");
+  }
+
+  const data = await res.json();
+  return NextResponse.json({ data });
+}
